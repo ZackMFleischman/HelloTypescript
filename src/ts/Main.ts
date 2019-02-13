a38dbec7ffb6339ddad95cc8ee9677f27f51cbf9
Zack M Fleischman <ZackMFleischman@gmail.com>
2019-02-13 09:52:14 -0500

    Users have been added and now display

--- src/ts/Main.ts
+++ src/ts/Main.ts
@@ -27,10 +27,54 @@ function setupWithData(websiteData: IWebsiteData) {
 
         const usersDiv: HTMLElement = document.createElement("div");
         usersDiv.innerText = "Users:";
+
+        usersDiv.appendChild(getUsersHTMLElement(websiteData.users));
+
         mainDiv.appendChild(usersDiv);
+
     } else {
         console.log(`Couldn't find the main div!`);
     }
+}
+
+function getUsersHTMLElement(users: IUser[]): HTMLElement {
+    const usersUL: HTMLOListElement = document.createElement("ol");
+
+    users.forEach(user => {
+        const listItem = document.createElement("li")
+        listItem.appendChild(getUserHTMLElement(user));
+
+        usersUL.appendChild(listItem);
+    })
+
+    return usersUL;
+}
+
+function getUserHTMLElement(user: IUser): HTMLElement {
+    const userUL: HTMLUListElement = document.createElement("ul");
+
+    userUL.appendChild(getListItemForString("Name: " + user.name));
+    userUL.appendChild(getListItemForString("Age: " + user.age));
+    userUL.appendChild(getListItemForString("Email: " + user.email));
+    userUL.appendChild(getListItemForString("Favorite Animal: " + user.favoriteAnimal));
+    userUL.appendChild(getListItemForImage(user.picture));
+
+    return userUL;
+}
+
+function getListItemForString(label: string) {
+    const listItem: HTMLLIElement = document.createElement("li");
+    listItem.innerText = label;
+    return listItem;
+}
+
+function getListItemForImage(imageUrl: string) {
+    const listItem: HTMLLIElement = document.createElement("li");
+    const imageElement: HTMLImageElement = document.createElement("img");
+    imageElement.src = imageUrl;
+
+    listItem.appendChild(imageElement);
+    return listItem;
 }
 
 const fetcher = new Fetcher();
