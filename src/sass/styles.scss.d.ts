e360e468988784a92d0b7d6c104a9aceb104ac17
Zack M Fleischman <ZackMFleischman@gmail.com>
2019-02-20 21:36:25 -0500

    Creating TicTacToe Bones

--- src/sass/styles.scss.d.ts
+++ src/sass/styles.scss.d.ts
@@ -1,1 +1,2 @@
 export const idMain: string;
+export const idUsers: string;
--- src/ts/Main.ts
+++ src/ts/Main.ts
@@ -26,6 +26,7 @@ function setupWithData(websiteData: IWebsiteData) {
         mainDiv.innerText = websiteData.title;
 
         const usersDiv: HTMLElement = document.createElement("div");
+        usersDiv.id = "idUsers";
         usersDiv.innerText = "Users:";
         mainDiv.appendChild(usersDiv);
 
@@ -34,6 +35,127 @@ function setupWithData(websiteData: IWebsiteData) {
     }
 }
 
+<<<<<<< Updated upstream
 const fetcher = new Fetcher();
 fetcher.fetchYaml<IWebsiteData>(Environment.configServer() + "yaml/example.yaml")
     .then(setupWithData);
+=======
+function getUsersHTMLElement(users: IUser[]): HTMLElement {
+    const usersUL: HTMLOListElement = document.createElement("ol");
+
+    users.forEach(user => {
+        const listItem = document.createElement("li")
+        listItem.appendChild(getUserHTMLElement(user));
+
+        usersUL.appendChild(listItem);
+    })
+
+    return usersUL;
+}
+
+function getUserHTMLElement(user: IUser): HTMLElement {
+    const userUL: HTMLUListElement = document.createElement("ul");
+
+    userUL.appendChild(getListItemForString("Name: " + user.name));
+    userUL.appendChild(getListItemForString("Age: " + user.age));
+    userUL.appendChild(getListItemForString("Email: " + user.email));
+    userUL.appendChild(getListItemForString("Favorite Animal: " + user.favoriteAnimal));
+    userUL.appendChild(getListItemForImage(user.picture));
+
+    return userUL;
+}
+
+function getListItemForString(label: string) {
+    const listItem: HTMLLIElement = document.createElement("li");
+    listItem.innerText = label;
+    return listItem;
+}
+
+function getListItemForImage(imageUrl: string) {
+    const listItem: HTMLLIElement = document.createElement("li");
+    const imageElement: HTMLImageElement = document.createElement("img");
+    imageElement.src = imageUrl;
+
+    listItem.appendChild(imageElement);
+    return listItem;
+}
+
+// Uncomment this for the original code...
+// const fetcher = new Fetcher();
+// fetcher.fetchYaml<IWebsiteData>(Environment.configServer() + "yaml/example.yaml")
+//     .then(setupWithData);
+
+
+
+interface GameModel {
+    grid2D: string[][];
+    nextPlayersTurn: string; // "x" or "o"
+}
+
+/// TIC - TAC - TOE
+/*
+let grid2D: string[][] = [
+        [x, o, _],
+        [_, x, o],
+        [x, o, _]
+    ];
+*/
+function displayGameBoard(gameModel: GameModel) {
+
+    console.log("Next Players Turn: " + gameModel.nextPlayersTurn);
+
+    for (let x = 0; x < 3; ++x) {
+
+        let displayRow: string = "";
+        for (let y = 0; y < 3; ++y) {
+            displayRow += gameModel.grid2D[x][y];
+        }
+        console.log(displayRow);
+
+    }
+}
+
+function playTicTacToe() {
+    console.log("Let's Play TicTacToe");
+
+    const x: string = "x";
+    const o: string = "o";
+    const _: string = " ";
+
+    let grid2D: string[][] = [
+        [x, o, _],
+        [_, x, o],
+        [x, o, _]
+    ];
+    displayGameBoard({
+        nextPlayersTurn: "x",
+        grid2D: grid2D
+    });
+
+    // x | o |  
+    // ---------
+    //   | x |  
+    // ---------
+    //   | o | x
+
+
+
+    // What do we need to play?
+    // 1. State of the board: Game Model
+    //      -- Data
+    // 2. Display the board   
+    //    -- Background-
+    //
+    // - 2 Players
+    // - Game rules
+    //     - Start game
+    // - End of game check and message, offer to restart the game.
+
+
+
+
+}
+
+playTicTacToe();
+
+>>>>>>> Stashed changes
