85b5763c8b8713ad22d544a8965d86891e6ca5d2
Zack M Fleischman <ZackMFleischman@gmail.com>
2019-03-03 21:37:24 -0500

    Updating TicTacToe with bones

--- src/ts/Main.ts
+++ src/ts/Main.ts
@@ -7,6 +7,7 @@ import Fetcher from "@Fetcher/Fetcher";
 import Environment from "@Environment/Environment";
 import CSS from "@Sass/styles.scss";
 
+
 interface IUser {
     name: string;
     age: number;
@@ -80,25 +81,82 @@ function getListItemForImage(imageUrl: string) {
 // fetcher.fetchYaml<IWebsiteData>(Environment.configServer() + "yaml/example.yaml")
 //     .then(setupWithData);
 
+///////////////////////////////////////////////////////
+///////////////////// TIC TAC TOE /////////////////////
+///////////////////////////////////////////////////////
 
 
+// x | o |  
+// ---------
+//   | x |  
+// ---------
+//   | o | x
+
+//
+// What we need to do in order (Game Recipe)
+//
+// 1. Declare the GameModel and initialize it with the initial state of the game.
+// 2. Enter a game loop:
+//      1. Display the Board to the player
+//      2. Ask the correct player for his move
+//      3. Apply his move to the game, and recalculate the next game state
+//      4. If a player has won/lost, exit the loop and show GameOver/YouWin.
+
+const x: string = "x";
+const o: string = "o";
+const _: string = " ";
+
+// Declare GameModel
 interface GameModel {
     grid2D: string[][];
     nextPlayersTurn: string; // "x" or "o"
 }
 
-/// TIC - TAC - TOE
+function playGame() {
+    // Step 1: Declare the GameModel and initialize it with the initial state of the game.
+    const gameModel: GameModel = getInitialGameModel();
+
+    // Step 2. Enter a game loop
+    while (true) {
+        // Step 2a. Display the Board to the player
+        displayGameBoard(gameModel);
+
+        // Step 2b. Ask the correct player for his move
+        const nextMove: number = promptNextPlayerForMove(gameModel.nextPlayersTurn);
+
+        // Step 2c. Apply his move to the game, and recalculate the next game state
+        applyMoveToGame(nextMove, gameModel);
+
+        // Step 2d. If a player has won/lost, exit the loop and show GameOver/YouWin.
+        if (isGameOver(gameModel)) {
+            break;
+        }
+    }
+}
+
+// Step 1: Initialize the GameModel with the initial state of the game
+function getInitialGameModel(): GameModel {
+    let grid2D: string[][] = [
+        [_, _, _],
+        [_, _, _],
+        [_, _, _]
+    ];
+
+    return {
+        nextPlayersTurn: x,
+        grid2D: grid2D
+    } as GameModel;
+}
+
 /*
+// Step 2a. Display the Board to the player
 let grid2D: string[][] = [
         [x, o, _],
         [_, x, o],
         [x, o, _]
     ];
 */
-function displayGameBoard(gameModel: GameModel) {
-
-    console.log("Next Players Turn: " + gameModel.nextPlayersTurn);
-
+function displayGameBoard(gameModel: GameModel): void {
     for (let x = 0; x < 3; ++x) {
 
         let displayRow: string = "";
@@ -106,49 +164,49 @@ function displayGameBoard(gameModel: GameModel) {
             displayRow += gameModel.grid2D[x][y];
         }
         console.log(displayRow);
-
     }
 }
 
-function playTicTacToe() {
-    console.log("Let's Play TicTacToe");
-
-    const x: string = "x";
-    const o: string = "o";
-    const _: string = " ";
-
-    let grid2D: string[][] = [
-        [x, o, _],
-        [_, x, o],
-        [x, o, _]
-    ];
-    displayGameBoard({
-        nextPlayersTurn: "x",
-        grid2D: grid2D
-    });
-
-    // x | o |  
-    // ---------
-    //   | x |  
-    // ---------
-    //   | o | x
-
-
-
-    // What do we need to play?
-    // 1. State of the board: Game Model
-    //      -- Data
-    // 2. Display the board   
-    //    -- Background-
-    //
-    // - 2 Players
-    // - Game rules
-    //     - Start game
-    // - End of game check and message, offer to restart the game.
-
-
-
-
+// Step 2b. Ask the correct player for his move
+// Current Game Board:
+// _ _ _  | 1 2 3
+// _ x _  | 4 5 6
+// _ _ _  | 7 8 9
+function promptNextPlayerForMove(nextPlayer: string): number {
+    // TODO: Prompt for input
+    return 5;
 }
 
-playTicTacToe();
+// Step 2c. Apply his move to the game, and recalculate the next game state
+function applyMoveToGame(nextMove: number, gameModel: GameModel): void {
+}
+
+// Step 2d
+function isGameOver(gameModel: GameModel): boolean {
+    // 1. Rows, Columns, or Diagonals are all one player
+    // 2. The board is full (tie)
+    return false;
+}
+
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+
+// TYPE SIGNATURE: It's inputs and outputs:
+// 1. INPUTS: Look in the parentheses, and see how many parameters there are, and what
+//    their types are.
+// 2. OUTPUTS: Look right after the parentheses, before the {, and see what it's return type
+//    is.
+function whatsMyTypeSignature(param1: string, param2: number): string {
+    return "hello";
+}
+
+
+// Type Signature
+// Inputs: 2 parameters: object, string
+// Outputs: 1 return value: number
+function whatsMyTypeSignature2(param1: object, param2: string): number {
+    return 99;
+}
+
+playGame();
