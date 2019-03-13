3498574fa08f4706b225220dc635e5215d364a17
Zack M Fleischman <ZackMFleischman@gmail.com>
2019-03-13 14:38:22 -0700

    Updating code to be a visual version of TicTacToe

--- src/ts/Main.ts
+++ src/ts/Main.ts
@@ -1,141 +1,57 @@
+///////////////////////////////////////////////////////
+////////////////////// IMPORTS ////////////////////////
+///////////////////////////////////////////////////////
+import "@Sass/styles";
+import { ENGINE_METHOD_PKEY_ASN1_METHS } from "constants";
+
+
 //
-// Entry Point into the app.
+// TIC-TAC-TOE
 //
+// Game Recipe:
+//
+// Step 1. Initialize gameState and game visuals.
+// Step 2. When the player makes a move (i.e. `playerMoved` method):
+//          a. Update the game state.
+//          b. Update the game visuals to reflect the new game state.
+//          c. Test if the game is over, and end the game if so.
 
-import "@Sass/styles"; // Pull in the CSS
-import Fetcher from "@Fetcher/Fetcher";
-import Environment from "@Environment/Environment";
-import CSS from "@Sass/styles.scss";
-
-
-interface IUser {
-    name: string;
-    age: number;
-    email: string;
-    favoriteAnimal: string;
-    picture: string;
-}
-
-interface IWebsiteData {
-    title: string;
-    users: IUser[];
-}
-
-function setupWithData(websiteData: IWebsiteData) {
-    const mainDiv: HTMLElement | null = document.getElementById(CSS.idMain);
-    if (mainDiv) {
-        mainDiv.innerText = websiteData.title;
-
-        const usersDiv: HTMLElement = document.createElement("div");
-        usersDiv.id = "idUsers";
-        usersDiv.innerText = "Users:";
-        mainDiv.appendChild(usersDiv);
-
-    } else {
-        console.log(`Couldn't find the main div!`);
-    }
-}
-
-function getUsersHTMLElement(users: IUser[]): HTMLElement {
-    const usersUL: HTMLOListElement = document.createElement("ol");
-
-    users.forEach(user => {
-        const listItem = document.createElement("li")
-        listItem.appendChild(getUserHTMLElement(user));
-
-        usersUL.appendChild(listItem);
-    })
-
-    return usersUL;
-}
-
-function getUserHTMLElement(user: IUser): HTMLElement {
-    const userUL: HTMLUListElement = document.createElement("ul");
-
-    userUL.appendChild(getListItemForString("Name: " + user.name));
-    userUL.appendChild(getListItemForString("Age: " + user.age));
-    userUL.appendChild(getListItemForString("Email: " + user.email));
-    userUL.appendChild(getListItemForString("Favorite Animal: " + user.favoriteAnimal));
-    userUL.appendChild(getListItemForImage(user.picture));
-
-    return userUL;
-}
-
-function getListItemForString(label: string) {
-    const listItem: HTMLLIElement = document.createElement("li");
-    listItem.innerText = label;
-    return listItem;
-}
-
-function getListItemForImage(imageUrl: string) {
-    const listItem: HTMLLIElement = document.createElement("li");
-    const imageElement: HTMLImageElement = document.createElement("img");
-    imageElement.src = imageUrl;
-
-    listItem.appendChild(imageElement);
-    return listItem;
-}
-
-// Uncomment this for the original code...
-// const fetcher = new Fetcher();
-// fetcher.fetchYaml<IWebsiteData>(Environment.configServer() + "yaml/example.yaml")
-//     .then(setupWithData);
 
 ///////////////////////////////////////////////////////
-///////////////////// TIC TAC TOE /////////////////////
+//////////////// GLOBALS DECLARATIONS /////////////////
 ///////////////////////////////////////////////////////
-
-
-// x | o |  
-// ---------
-//   | x |  
-// ---------
-//   | o | x
-
-//
-// What we need to do in order (Game Recipe)
-//
-// 1. Declare the GameModel and initialize it with the initial state of the game.
-// 2. Enter a game loop:
-//      1. Display the Board to the player
-//      2. Ask the correct player for his move
-//      3. Apply his move to the game, and recalculate the next game state
-//      4. If a player has won/lost, exit the loop and show GameOver/YouWin.
-
 const x: string = "x";
 const o: string = "o";
 const _: string = " ";
 
-// Declare GameModel
-interface GameModel {
+// Declare GameState data structure
+interface GameState {
     grid2D: string[][];
     nextPlayersTurn: string; // "x" or "o"
 }
 
-function playGame() {
-    // Step 1: Declare the GameModel and initialize it with the initial state of the game.
-    const gameModel: GameModel = getInitialGameModel();
+// Global variable representing the current GameState.
+let gameState: GameState;
 
-    // Step 2. Enter a game loop
-    while (true) {
-        // Step 2a. Display the Board to the player
-        displayGameBoard(gameModel);
+// Store all the TicTacToe cells (which are HTML Divs) in a global array.
+let ticTacToeCells: HTMLDivElement[];
 
-        // Step 2b. Ask the correct player for his move
-        const nextMove: number = promptNextPlayerForMove(gameModel.nextPlayersTurn);
+// This is a reference to the `div` that says "X goes first"
+let label: HTMLDivElement = document.getElementById("label")! as HTMLDivElement;
 
-        // Step 2c. Apply his move to the game, and recalculate the next game state
-        applyMoveToGame(nextMove, gameModel);
 
-        // Step 2d. If a player has won/lost, exit the loop and show GameOver/YouWin.
-        if (isGameOver(gameModel)) {
-            break;
-        }
-    }
+///////////////////////////////////////////////////////
+//////////////// GAME LOGIC ///////////////////////////
+///////////////////////////////////////////////////////
+
+
+// [Step 1]
+function initializeGame() {
+    gameState = getInitialGameState();
+    initializeGameVisuals();
 }
 
-// Step 1: Initialize the GameModel with the initial state of the game
-function getInitialGameModel(): GameModel {
+function getInitialGameState(): GameState {
     let grid2D: string[][] = [
         [_, _, _],
         [_, _, _],
@@ -145,68 +61,67 @@ function getInitialGameModel(): GameModel {
     return {
         nextPlayersTurn: x,
         grid2D: grid2D
-    } as GameModel;
+    } as GameState;
 }
 
-/*
-// Step 2a. Display the Board to the player
-let grid2D: string[][] = [
-        [x, o, _],
-        [_, x, o],
-        [x, o, _]
-    ];
-*/
-function displayGameBoard(gameModel: GameModel): void {
-    for (let x = 0; x < 3; ++x) {
+function initializeGameVisuals() {
+    ticTacToeCells = []; // Declare this as an empty array to start, then add to it.
+    ticTacToeCells.push(document.getElementById("row1_column1")! as HTMLDivElement);
+    ticTacToeCells.push(document.getElementById("row1_column2")! as HTMLDivElement);
+    ticTacToeCells.push(document.getElementById("row1_column3")! as HTMLDivElement);
+    ticTacToeCells.push(document.getElementById("row2_column1")! as HTMLDivElement);
+    ticTacToeCells.push(document.getElementById("row2_column2")! as HTMLDivElement);
+    ticTacToeCells.push(document.getElementById("row2_column3")! as HTMLDivElement);
+    ticTacToeCells.push(document.getElementById("row3_column1")! as HTMLDivElement);
+    ticTacToeCells.push(document.getElementById("row3_column2")! as HTMLDivElement);
+    ticTacToeCells.push(document.getElementById("row3_column3")! as HTMLDivElement);
 
-        let displayRow: string = "";
-        for (let y = 0; y < 3; ++y) {
-            displayRow += gameModel.grid2D[x][y];
-        }
-        console.log(displayRow);
+    // For each cell, let's make sure the cell is empty, and when the player clicks it,
+    // it will call the `playerMoved` function with that cell number.
+    for (let i = 0; i < ticTacToeCells.length; ++i) {
+        ticTacToeCells[i].innerText = _;
+        ticTacToeCells[i].onclick = ((mouseEvent: MouseEvent) => playerMoved(i));
     }
 }
 
-// Step 2b. Ask the correct player for his move
-// Current Game Board:
-// _ _ _  | 1 2 3
-// _ x _  | 4 5 6
-// _ _ _  | 7 8 9
-function promptNextPlayerForMove(nextPlayer: string): number {
-    // TODO: Prompt for input
-    return 5;
+// [Step 2]
+// This gets called when the player clicks on a cell.
+function playerMoved(cellNumber: number) {
+    updateGameStateWithPlayerMove(cellNumber);
+    updateGameVisuals();
+
+    // [Step 2c]
+    if (isGameOver())
+        endGame();
 }
 
-// Step 2c. Apply his move to the game, and recalculate the next game state
-function applyMoveToGame(nextMove: number, gameModel: GameModel): void {
+// [Step 2a]
+function updateGameStateWithPlayerMove(cellNumber: number) {
+    // TODO: update the `gameState` variable accordingly here.
+    //       based on tic-tac-toe logic.
 }
 
-// Step 2d
-function isGameOver(gameModel: GameModel): boolean {
+// [Step 2b]
+function updateGameVisuals() {
+    // TODO: Change the visuals of the game board via the `ticTacToeCells` array
+    //       to look like the current `gameState`. 
+    //       Hint: use `innerText` on a div.
+}
+
+
+function isGameOver(): boolean {
+    // TODO: Implement this function using the global `gameState` variable
+    //       to figure out if the game is over or not.
+
+    // HINT: The game is over if either of these are true.
     // 1. Rows, Columns, or Diagonals are all one player
     // 2. The board is full (tie)
     return false;
 }
 
-
-////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////
-
-// TYPE SIGNATURE: It's inputs and outputs:
-// 1. INPUTS: Look in the parentheses, and see how many parameters there are, and what
-//    their types are.
-// 2. OUTPUTS: Look right after the parentheses, before the {, and see what it's return type
-//    is.
-function whatsMyTypeSignature(param1: string, param2: number): string {
-    return "hello";
+function endGame(): void {
+    // TODO: Inform the players the game is over and who won. 
+    //       Bonus points if you restart for the next game! 
 }
 
-
-// Type Signature
-// Inputs: 2 parameters: object, string
-// Outputs: 1 return value: number
-function whatsMyTypeSignature2(param1: object, param2: string): number {
-    return 99;
-}
-
-playGame();
+initializeGame();
---
+++ src/ts/ScratchPad.ts
@@ -0,0 +1,23 @@
+//
+// This is just a Typescript file to dump throw-away code and historical learnings.
+//
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+
+// TYPE SIGNATURE: It's inputs and outputs:
+// 1. INPUTS: Look in the parentheses, and see how many parameters there are, and what
+//    their types are.
+// 2. OUTPUTS: Look right after the parentheses, before the {, and see what it's return type
+//    is.
+function whatsMyTypeSignature(param1: string, param2: number): string {
+    return "hello";
+}
+
+
+// Type Signature
+// Inputs: 2 parameters: object, string
+// Outputs: 1 return value: number
+function whatsMyTypeSignature2(param1: object, param2: string): number {
+    return 99;
+}
